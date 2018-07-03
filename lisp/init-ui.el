e9bedfef702cf28960ce3510f4aa1ae7420610a4
moonwwdz <wangy3483@gmail.com>
2018-07-02 21:35:21 -1000

    gui display bar

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -1,3 +1,9 @@
+(when (display-graphic-p)
+  ;; 关闭工具栏，tool-bar-mode 即为一个 Minor Mode
+  (tool-bar-mode -1)
+
+  ;; 关闭文件滑动控件
+  (scroll-bar-mode -1))
 
 ;; 关闭工具栏，tool-bar-mode 即为一个 Minor Mode
 ;;(tool-bar-mode -1)
