8dfcc2f8aa6df45d47dda891235108209f70eb56
moonwwdz <wangy3483@gmail.com>
2022-09-11 03:18:48 +0000

    修改库地址

--- lisp/init-packages.el
+++ lisp/init-packages.el
@@ -7,8 +7,8 @@
 (when (>= emacs-major-version 24)
   (require 'package)
   (package-initialize)
-  (setq package-archives '(("gnu"   . "http://elpa.emacs-china.org/gnu/")
-			   ("melpa" . "http://elpa.emacs-china.org/melpa/"))))
+  (setq package-archives '(("gnu"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
+			   ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/"))))
 
 ;; cl - Common Lisp Extension
 (require 'cl)
