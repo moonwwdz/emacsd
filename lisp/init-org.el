5770e4d3270082171ec573c7b1e063306f8b81c6
moonwwdz <wangy3483@gmail.com>
2024-02-26 10:52:42 +0800

    update roam templates

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -20,7 +20,7 @@
 ;;(add-to-list 'org-roam-capture-templates
 ;;             '("d" "Default" plain "%?" :target (file+head "%<%Y%m%d%H>-${slug}.org" "#+title: ${title}\n#+filetags: \n")))
 (setq org-roam-capture-templates '(("d" "default" plain "%?" :target (file+head "%<%Y%m%d%H>-${slug}.org" "#+STARTUP: content\n#+title: ${title}\n#+filetags: \n") :unnarrowed t)
-                ("b" "Book/Article/Movie" plain "%?" :target (file+head "Collection/collection%<%Y%m%d%H>-${slug}.org" "#+STARTUP: content\n#+title: ${title}\n#+filetags: :bookreading: \n\n") :unnarrowed t)
+                ("b" "Book/Article/Movie" plain "%?" :target (file+head "Collection/collection%<%Y%m%d%H>-${slug}.org" "#+STARTUP: content\n#+title: ${title}\n#+links: \n#+filetags: :bookreading: \n\n** Original Data") :unnarrowed t)
                 ("k" "Knowledge" plain "%?" :target (file+head "Knowledge/knowledge%<%Y%m%d%H>-${slug}.org" "#+STARTUP: content\n#+title:${slug}\n#+links: \n#+filetags: :knowledge: \n\n** Original Data ") :unnarrowed t)
                 ;;("c" "company" plain "%?" :target (file+head "company/company%<%Y%m%d%H>-${slug}.org" "#+title: ${title}\n#filetags: :compnay: \n\n") :unnarrowed t)
 		("c" "Coding" plain "%?" :target (file+head "marketing/marketing%<%Y%m%d%H%M%S>-${slug}.org" "#+STARTUP: content\n#+title: ${title}\n#+filetags: :marketing: \n\n") :unnarrowed t)
